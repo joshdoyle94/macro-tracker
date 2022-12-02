1c1fcb2248333c736144d417fad8736dd9dd4cdf
joshdoyle94 <joshdoyle94@gmail.com>
2022-12-01 23:49:58 -0500

    nothing new

---
+++ macro-tracker-client/src/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl
+const apiUrls = {
+	production: '<replace_with_deployed_api_url>',
+	development: 'http://localhost:8000',
+}
+
+if (window.location.hostname === 'localhost') {
+	apiUrl = apiUrls.development
+} else {
+	apiUrl = apiUrls.production
+}
+
+export default apiUrl
